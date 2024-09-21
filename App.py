b5e2495d7a01c9705e6c9bbba4d73fd583014a24
Karishma003 <147496774+Karishma003@users.noreply.github.com>
2024-09-21 22:30:08 +0530

    Update App.py

--- App.py
+++ App.py
@@ -16,7 +16,7 @@ st.sidebar.title("Settings")
 # Load data
 @st.cache_data
 def load_data():
-    data = pd.read_csv(r"C:\Users\karis\Documents\breast_cancer.csv")
+    data = pd.read_csv("breast-cancer[1].csv")
     return data
 
 data = load_data()
